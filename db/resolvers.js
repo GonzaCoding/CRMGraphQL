a3e45bf77798721fabf7cba21b1e77cb9441f52a
giglesias <giglesias@appsolut.us>
2020-11-13 19:09:35 -0300

    crud clientes

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,5 +1,6 @@
 const Usuario = require('../models/usuario');
 const Producto = require('../models/producto');
+const Cliente = require('../models/cliente');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path: ".env"});
@@ -18,6 +19,61 @@ const resolvers = {
             const usuarioID = jwt.verify(token, process.env.SECRET);
 
             return usuarioID;
+        },
+        obtenerProductos: async () => {
+            try {
+
+                const productos = await Producto.find({});
+                return productos;
+
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        obtenerProducto: async (_, {id}) => {
+            //revisar si existe
+            const producto = await Producto.findById(id);
+
+            if(!producto) {
+                throw new Error("Producto no encontrado");
+            }
+
+            return producto;
+        },
+        obtenerClientes: async () => {
+            try {
+
+                const clientes = await Cliente.find({});
+                return clientes;
+
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        obtenerClientesVendedor: async (_, {}, ctx) => {
+            try {
+
+                const clientes = await Cliente.find({vendedor: ctx.usuario.id.toString()});
+                return clientes;
+
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        obtenerCliente: async (_, {id}, ctx) => {
+            //revisar si el cliente existe
+            const cliente = await Cliente.findById(id);
+
+            if(!cliente){
+                throw new Error("Cliente no encontrado");
+            }
+
+            //quien lo creo puede verlo
+            if(cliente.vendedor.toString() !== ctx.usuario.id) {
+                throw new Error("El cliente es de otro usuario, no puedes verlo");
+            }
+
+            return cliente;
         }
     },
     Mutation: {
@@ -77,6 +133,92 @@ const resolvers = {
             } catch (err) {
                 console.log(err);
             }
+        },
+        actualizarProducto: async (_,{id, input}) => {
+            //revisar si existe
+            let producto = await Producto.findById(id);
+
+            if(!producto) {
+                throw new Error("Producto no encontrado");
+            }
+
+            // guardar en la BD
+            producto = await Producto.findOneAndUpdate({_id: id}, input, {new: true});
+
+            return producto;
+
+        },
+        eliminarProducto: async (_, {id}) => {
+            //revisar si existe
+            let producto = await Producto.findById(id);
+
+            if(!producto) {
+                throw new Error("Producto no encontrado");
+            }
+
+            // eliminar
+            await Producto.findByIdAndDelete({_id: id});
+
+            return "Producto eliminado";
+        },
+        nuevoCliente: async (_, {input}, ctx) => {
+            //verificar si está registrado
+            const {email} = input;
+
+            const cliente = await Cliente.findOne({email});
+
+            if(cliente){
+                throw new Error("Cliente ya existe");
+            }
+
+            const nuevoCliente = new Cliente(input);
+
+            //asignar vendedor
+            nuevoCliente.vendedor = ctx.usuario.id;
+
+            //guardar en BD
+            try {
+                const resultado = await nuevoCliente.save();
+
+                return resultado;
+            } catch (err) {
+                console.log(err);
+            }
+            
+        },
+        actualizarCliente: async (_, {id, input}, ctx) => {
+            //verificar si existe
+            let cliente = await Cliente.findById(id);
+
+            if(!cliente){
+                throw new Error("El cliente no existe");
+            }
+
+            //verificar si es el vendedor el que edita
+            if(cliente.vendedor.toString() !== ctx.usuario.id) {
+                throw new Error("El cliente es de otro usuario, no puedes editarlo");
+            }
+
+            //guardar
+            cliente = await Cliente.findOneAndUpdate({_id: id}, input, {new: true});
+            return cliente;
+        },
+        eliminarCliente: async (_, {id}, ctx) => {
+            //verificar si existe
+            let cliente = await Cliente.findById(id);
+
+            if(!cliente){
+                throw new Error("El cliente no existe");
+            }
+
+            //verificar si es el vendedor el que edita
+            if(cliente.vendedor.toString() !== ctx.usuario.id) {
+                throw new Error("El cliente es de otro usuario, no puedes editarlo");
+            }
+
+            //eliminar
+            await Cliente.findOneAndDelete({_id: id});
+            return "Cliente eliminado";
         }
     }
 
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 const conectarDB = require('./config/db');
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: ".env"});
 
 // conectar BD
 conectarDB();
@@ -10,7 +11,26 @@ conectarDB();
 // crear servidor
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({req}) => {
+        const token = req.headers['authorization'] || '';
+
+        if(token) {
+            try {
+                const usuario = jwt.verify(token, process.env.SECRET);
+
+                //console.log(usuario);
+
+                return {
+                    usuario,
+                };
+            } catch (err) {
+                console.log("Hubo un error");
+                console.log(err);
+            }
+        }
+
+    }
 });
 
 // arrancar el servidor
