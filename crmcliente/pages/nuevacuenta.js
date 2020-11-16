7343c3a5f3d8c8ca32e307d8cded70eb71379620
giglesias <giglesias@appsolut.us>
2020-11-16 19:16:19 -0300

    layout de componentes login y nuevacuenta

---
+++ crmcliente/pages/nuevacuenta.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NuevaCuenta = () => {
+    return (
+        <Layout>
+            <h1>NuevaCuenta</h1>
+        </Layout>
+    )
+}
+
+export default NuevaCuenta;
