04c57ed4db4ee05275525489f62612ed11b92882
GonzaCoding <iglesias.gh@gmail.com>
2021-07-16 18:29:10 -0300

    Eliminar productos

---
+++ crmcliente/components/Producto.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+import { gql, useMutation } from '@apollo/client';
+
+const ELIMINAR_PRODUCTO = gql`
+
+    mutation eliminarProducto($id: ID!) {
+        eliminarProducto(id: $id)
+    }
+
+`;
+
+const OBTENER_PRODUCTOS = gql`
+
+    query obtenerProductos {
+        obtenerProductos {
+            id
+            nombre
+            precio
+            existencia
+        }
+    }
+
+`;
+
+const Producto = ({producto}) => {
+    const { nombre, id, precio, existencia } = producto;
+
+    const [ eliminarProducto ] = useMutation(ELIMINAR_PRODUCTO, {
+        update(cache) {
+            const { obtenerProductos } = cache.readQuery({
+                query: OBTENER_PRODUCTOS,
+            });
+
+            cache.writeQuery({
+                query: OBTENER_PRODUCTOS,
+                data: {
+                    obtenerProductos: obtenerProductos.filter( productoActual => productoActual.id !== id)
+                }
+            })
+        }
+    });
+
+    const confirmarEliminarProducto = () => {
+        Swal.fire({
+            title: "¿Deseas eliminar este producto?",
+            text: "Esta acción no se puede deshacer!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sí, eliminar!",
+            cancelButtonText: 'No, cancelar'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    const { data } = await eliminarProducto({
+                        variables: {
+                            id
+                        }
+                    })
+
+                    Swal.fire(
+                        'Correcto',
+                        data.eliminarProducto,
+                        'success'
+                    );
+                    
+                } catch (error) {
+                    console.log(error);
+                }  
+                
+                
+            }
+        });
+    }
+
+    return (
+        <tr>
+            <td className="border px-4 py-2">{nombre}</td>
+            <td className="border px-4 py-2">{existencia} piezas</td>
+            <td className="border px-4 py-2">$ {precio}</td>
+            <td className="border px-4 py-2">
+                <button
+                    type="button"
+                    className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    onClick={()=> confirmarEliminarProducto()}
+                >
+                    Eliminar
+                    <svg className="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+            </td>
+            <td className="border px-4 py-2">
+                <button
+                    type="button"
+                    className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    //onClick={()=> editarCliente()}
+                >
+                    Editar
+                    <svg className="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+export default Producto;
