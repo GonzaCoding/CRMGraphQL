9791f7c62c82e8baeb0f98aabb4ae37d596040df
GonzaCoding <iglesias.gh@gmail.com>
2021-07-16 21:12:14 -0300

    Crear productos

--- crmcliente/pages/productos.js
+++ crmcliente/pages/productos.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout';
 import Producto from '../components/Producto';
 import { gql, useQuery } from '@apollo/client';
+import Link from 'next/link';
 
 const OBTENER_PRODUCTOS = gql`
 
@@ -27,6 +28,12 @@ const Productos = () => {
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+
+            <Link href="/nuevoproducto">
+                <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white hover:bg-gray-800 hover:text-gray-200 mb-3 rounded uppercase font-bold test-sm">
+                    Nuevo Producto
+                </a>
+            </Link>
 
             <table className="table-auto shadow-md mt-10 w-full w-lg ">
                 <thead className="bg-gray-800">
