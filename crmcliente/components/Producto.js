decc10d12c23ff7f8f282ed09519cf91ff1c2fdf
GonzaCoding <iglesias.gh@gmail.com>
2021-07-20 17:32:27 -0300

    Editar Producto

--- crmcliente/components/Producto.js
+++ crmcliente/components/Producto.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 import { gql, useMutation } from '@apollo/client';
+import Router from 'next/router';
 
 const ELIMINAR_PRODUCTO = gql`
 
@@ -75,6 +76,13 @@ const Producto = ({producto}) => {
         });
     }
 
+    const editarProducto = () => {
+        Router.push({
+            pathname:"/editarproducto/[id]",
+            query: { id }
+        });
+    }
+
     return (
         <tr>
             <td className="border px-4 py-2">{nombre}</td>
@@ -94,7 +102,7 @@ const Producto = ({producto}) => {
                 <button
                     type="button"
                     className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-                    //onClick={()=> editarCliente()}
+                    onClick={()=> editarProducto()}
                 >
                     Editar
                     <svg className="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
